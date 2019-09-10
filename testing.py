7ea4cbe2ec31d2cca5abff4764e540d996313043
MidgeWilliams <43554894+MidgeWilliams@users.noreply.github.com>
2019-09-10 18:46:56 -0400

    Work on assignment 1

--- testing.py
+++ testing.py
@@ -1,3 +1,27 @@
-import nltk
-import numpy as np
-import pandas as pd
+
+vowels = ['a','e','i','o','u']
+
+def has_most_consonants(text):
+    text_words = text.split()
+    cons_to_full = {}
+    
+    for word in text_words:
+        cons_only = word.lower()
+        for vowel in vowels:
+            cons_only = cons_only.replace(vowel, '')
+        cons_to_full[cons_only] = word
+
+    max_cons = len(max(cons_to_full.keys(), key=len))
+    final_list = []
+
+    for key in cons_to_full.keys():
+        if len(key) == max_cons:
+            final_list.append(cons_to_full[key])
+
+    return final_list
+
+
+
+p5str = 'The door is closed. And many dogs barked.'
+p5 = has_most_consonants(p5str)
+print(p5)
